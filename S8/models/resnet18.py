a8e5dbb836cdcdcc89fc5c454eed9ef2c1b464f5
Anurag Agarwal <anurag26@gmail.com>
2020-05-24 03:38:51 +0530

    Update resnet18.py

--- S8/models/resnet18.py
+++ S8/models/resnet18.py
@@ -2,3 +2,18 @@ from resnetbase import BasicBlock
 
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def ResNet101():
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
+
+def ResNet152():
+    return ResNet(Bottleneck, [3, 8, 36, 3])
