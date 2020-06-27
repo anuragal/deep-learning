ba633490d2c817352c58e55853b9f0a12a444711
Anurag Agarwal <anurag.agarwal@dell.com>
2020-06-27 09:25:05 +0530

    Added init.py and formatting for dataloader

--- S12/dataloader.py
+++ S12/dataloader.py
@@ -1,3 +1,5 @@
+from os import path
+
 import torch
 import torchvision
 
@@ -5,40 +7,79 @@ from transformation import TransformationFactory
 
 class ImageData(object):
 
-  classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog',
-			'frog', 'horse', 'ship', 'truck')
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog',
+		    'frog', 'horse', 'ship', 'truck')
 
-  def __init__(self):
-    super(ImageData, self).__init__()
-    self.trainloader = None
-    self.testloader = None
+    def __init__(self):
+        super(ImageData, self).__init__()
+        self.trainloader = None
+        self.testloader = None
+        self.cuda = None
 
-  def load(self, transformation_type="pytorch"):
-   # Choose from "albumentations" or "pytorch". Default is "pytorch"
-   t = TransformationFactory(transformation_type)
-   train_transform = t.load(is_train=True)
-   test_transform = t.load(is_train=False)
+    def load(self, transformation_type="pytorch"):
+        # Choose from "albumentations" or "pytorch". Default is "pytorch"
+        t = TransformationFactory(transformation_type)
+        train_transform = t.load(is_train=True)
+        test_transform = t.load(is_train=False)
 
-   SEED = 1
+        SEED = 1
 
-   # CUDA?
-   cuda = torch.cuda.is_available()
-   print("CUDA Available?", cuda)
+        # CUDA?
+        self.cuda = False # torch.cuda.is_available()
+        print("CUDA Available?", self.cuda)
 
-   # For reproducibility
-   torch.manual_seed(SEED)
+        # For reproducibility
+        torch.manual_seed(SEED)
 
-   if cuda:
-     torch.cuda.manual_seed(SEED)
+        if self.cuda:
+            torch.cuda.manual_seed(SEED)
 
-   # dataloader arguments - something you'll fetch these from cmdprmt
-   dataloader_args = dict(shuffle=True, batch_size=128, num_workers=4, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
+        return train_transform, test_transform
 
-   trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=train_transform)
+    def load_CIFAR(self, transformation_type="pytorch"):
+        train_transform, test_transform = self.load(transformation_type)
 
-   testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=True, transform=test_transform)
+        # dataloader arguments - something you'll fetch these from cmdprmt
+        dataloader_args = dict(shuffle=True, batch_size=128, num_workers=4, pin_memory=True) if self.cuda else dict(shuffle=True, batch_size=64)
 
-   self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=4)
-   self.testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=4)
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=train_transform)
+
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                            download=True, transform=test_transform)
+
+        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=4)
+        self.testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=4)
+
+    def load_TINY_IMAGENET(self, image_path, transformation_type="pytorch"):
+        train_transform, test_transform = self.load(transformation_type)
+
+        training_images = path.join(image_path, "train/")
+        test_images = path.join(image_path, "val/")
+
+        trainset = torchvision.datasets.ImageFolder(
+                              root = training_images,
+                              transform = train_transform
+                       )
+
+        testset = torchvision.datasets.ImageFolder(
+                              root = test_images,
+                              transform = test_transform
+                       )
+
+        #print(self.get_class_distribution(trainset))
+        #print(self.get_class_distribution(testset))
+
+        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=4)
+        self.testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=4)
+
+    def get_class_distribution(self, dataset_obj):
+        idx2class = {v: k for k, v in dataset_obj.class_to_idx.items()}
+        count_dict = {k:0 for k,v in dataset_obj.class_to_idx.items()}
+
+        for element in dataset_obj:
+            y_lbl = element[1]
+            y_lbl = idx2class[y_lbl]
+            count_dict[y_lbl] += 1
+
+        return count_dict
--- models/resnet18.py
+++ models/resnet18.py
@@ -1,4 +1,4 @@
 from models.resnetbase import BasicBlock, ResNet
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(num_classes=10):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=10)
